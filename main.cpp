340312d9890f6dd14d4ad11905b7a004bf3f23a8
sulgan420 <95031006+sulgan420@users.noreply.github.com>
2022-04-01 15:30:54 +0200

    Initial commit (ignoring commit "GUI basics" before)
    GUI basics was causing severe problems with git usage in QT creator, so I decided to create new project with right git setup and then just copy things from that one before

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@
 
 int main(int argc, char *argv[])
 {
-    //zmena pre git 2
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Calculator is Fetylegg");
     w.show();
     return a.exec();
 }
