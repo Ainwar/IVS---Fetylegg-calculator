3f6fc42fab79cb5b288c1430ed62561dbd9609a1
MatusTabi <95317725+MatusTabi@users.noreply.github.com>
2022-04-13 13:44:09 +0200

    Add files via upload

---
+++ projekt/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/matus/projekt
+# Build directory: /home/matus/projekt
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
