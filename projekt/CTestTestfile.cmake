93207d4cee5a47240a4ddc4d1ff455db9bc7fa71
MatusTabi <95317725+MatusTabi@users.noreply.github.com>
2022-04-24 00:16:55 +0200

    Delete projekt directory

--- projekt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matus/projekt
-# Build directory: /home/matus/projekt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
